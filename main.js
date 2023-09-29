a25faacdd5ca064ca08c3dc7021b4db835e0c2a9
NellyOniw <146453793+NellyOniw@users.noreply.github.com>
2023-09-29 12:23:08 -0300

    k

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ const lista = document.querySelectorAll('.tecla');
 let contador = 0
 
 while (contador < lista.length) {
-    const tecla = lista[contador];
-    const classe = tecla.classList[1];
+const tecla = lista[contador];
+const classe = tecla.classList[1];
 //console.log(classe);
 
 const idAudio = `#som_${classe}`;
